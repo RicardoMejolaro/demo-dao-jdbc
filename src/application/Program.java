8339fc88728a503eb1556e87839ef065142f83f1
RicardoMejolaro <ricardo.mejolaro@gmail.com>
2019-11-14 10:42:35 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -39,5 +40,13 @@ public class Program {
 			
 			System.out.println(obj);
 		}
+		
+		System.out.println();
+
+		System.out.println("Teste 4: seller Insert ----------");
+		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted new id: " + newSeller.getId());
 	}
 }
